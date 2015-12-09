52e8591b0b27307a52e487f4560e058f0a21bb6a
Blixa Morgan <blixa@projectmakeit.com>
2015-12-08 18:59:18 -0600

    rolling in some error and output trapping

--- init.sh
+++ init.sh
@@ -2,15 +2,32 @@
 
 function issues {
 	if [ "$1" == "init" ]; then
-		if git rev-parse --verify "_issues"; then
+		if hasIssuesBranch; then
 			echo 'the _issues branch already exists';
 		else
 			branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 			branch_name=${branch_name##refs/heads/}
-			git checkout --orphan "_issues";
-			git rm -rf *;
-			git commit --allow-empty -m 'initial commit'
-			git checkout $branch_name;
+			git checkout --orphan "_issues" 2> /dev/null > /dev/null;
+			git rm -rf * 2> /dev/null > /dev/null;
+			git commit --allow-empty -m 'initial commit' > /dev/null;
+			git checkout $branch_name 2> /dev/null > /dev/null;
+			echo 'Empty issues set up'
 		fi
+	else
+		if ! [ hasIssuesBranch ]; then
+			echo 'Issues has not be initalized. Please run ${0} init';
+		else
+			if [ "$1" == "create" ]; then
+				:
+			fi
+		fi
+	fi
+}
+
+function hasIssuesBranch {
+	if git rev-parse --verify "_issues" 2> /dev/null; then
+		return 0;
+	else
+		return 1;
 	fi
 }
